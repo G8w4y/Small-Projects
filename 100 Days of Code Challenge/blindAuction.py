7c3d50e70971cb8552230045cc1f25ba7b90ead3
G8w4y <tengelfredheim@gmail.com>
2022-04-22 08:36:00 +0200

    prøver meg frem med lister og dictionaries

--- 100 Days of Code Challenge/blindAuction.py
+++ 100 Days of Code Challenge/blindAuction.py
@@ -1,20 +1,27 @@
+import os
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
 biddersAndBids = []
 
 def add_bidder(name,bidValue):
-    import os
-    if(os.name == 'posix'):
-        os.system('clear')
-    else:
-        os.system('cls')
-    
+    clear()
+    bidder = []
+    bids = []
     newBidderWithBid = {}
-    newBidderWithBid["Bidder"] = ¨str(name)
+    newBidderWithBid["Bidder"] = str(name)
     newBidderWithBid["Bid"] = int(bidValue)
-    biddersAndBids.append(newBidderWithBid)
+    bidder, bids = zip(newBidderWithBid.items())
+    print(bidder)
+    print(f"Your bid is {bids}NOK")
+
+name = input(f"input your name: ")
+bidValue = input(f"you much would you like to bid?")
+add_bidder(name,bidValue)
+
 
 # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 # https://www.adamsmith.haus/python/answers/how-to-find-the-max-value-in-a-dictionary-in-python
 
-def winning_bid():
-    for element in biddersAndBids:
+# def winning_bid():
+#     for element in biddersAndBids:
 

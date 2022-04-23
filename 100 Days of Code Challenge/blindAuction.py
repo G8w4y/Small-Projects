d27b468e7b687cd29f6f322b47c107fa076b6d6f
G8w4y <Tengelfredheim@gmail.com>
2022-04-23 15:44:53 +0200

    Fullført stille auksjonsskriptet - debugging

--- 100 Days of Code Challenge/blindAuction.py
+++ 100 Days of Code Challenge/blindAuction.py
@@ -11,13 +11,11 @@ def add_bidder(name,bidValue):
     newBidderWithBid = {}
     newBidderWithBid["Bidder"] = str(name)
     newBidderWithBid["Bid"] = int(bidValue)
-    #bidder, bids = zip(newBidderWithBid.items())
-    #print(bidder)
-    #print(f"Your bid is {bids}NOK")
+
     print(newBidderWithBid)
-    print(biddersAndBids)
+    #print(biddersAndBids)
     biddersAndBids.append(newBidderWithBid)
-    print(biddersAndBids)
+    #print(biddersAndBids)
 
 
 not_exit = True
@@ -29,6 +27,7 @@ while not_exit:
     add_bidder(name,bidValue)
     not_finished = input("Type yes to add another bidder, and no to show the end results.\n").lower()
     if "y" in not_finished or "ye" in not_finished or "yes" in not_finished:
+        clear()
         continue
     else:
         not_exit = False
@@ -37,16 +36,33 @@ while not_exit:
 highest_bid = max(biddersAndBids, key=lambda x:x["Bid"])
 winning_bidder = highest_bid["Bidder"]
 winning_bid = highest_bid["Bid"]
-print(highest_bid)
-print(winning_bidder)
-print(winning_bid)
-print(f"{winning_bidder} had the highest bid of {winning_bid} NOK!\n\nOverview of all bids:")
-for index in biddersAndBids:
-    print(biddersAndBids[index]["Bidder"])
+## For testing
+#print(highest_bid)
+#print(winning_bidder)
+#print(winning_bid)
+clear()
+print("####################################################################")
+print(f"{winning_bidder} had the highest bid of {winning_bid},- NOK!\n\nOverview of all bids in descending order:")
 
 
+# Tester ut om vi kan sortere listen fra høyeste til laveste bud, slik at den blir penere å se på for folk
+biddersAndBids = sorted(biddersAndBids, key=lambda d: d["Bid"], reverse=True)
+### Koden under gjør det samme som koden over men vha et annet bibliotek.
+#from operator import itemgetter
+#biddersAndBids = sorted(biddersAndBids, key=itemgetter('Bid'), reverse=True)
 
+# Går igjennom hvert element i listen og printer listeplassen +1, deretter henter ut Key:value parene ved hver posisjon i dictionariene
+# Hver dictionary printes ut ut, før programmet hopper videre til neste element i listen og gjør det samme
+# Dette foregår frem til listens slutt
+for i, list_item in enumerate(biddersAndBids):
+    print(f"{i + 1}. ", end='')
+    for j, key in enumerate(list_item.keys()):
+        print(f"{key}:{list_item[key]}{'' if j == len(list_item) - 1 else ', '}", end='')
+        #print("")
+    print("")
+print("####################################################################")
 
+#{key}:{list_item[key]}
 # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 # https://www.adamsmith.haus/python/answers/how-to-find-the-max-value-in-a-dictionary-in-python
 

e500d98cc1995b571d48b02921c8551445948979
G8w4y <Tengelfredheim@gmail.com>
2022-05-13 07:17:33 +0200

    getting somewhere!

--- 100 Days of Code Challenge/blackJackV1.py
+++ 100 Days of Code Challenge/blackJackV1.py
@@ -2,15 +2,15 @@ import random
 import sys
 
 player_hand = []
-bot_hand = []
-bot_hidden_hand = ["X","X","X","X","X","X","X",]
-bot_shown_hand = []
+house_hand = []
+house_hidden_hand = ["X","X","X","X","X","X","X",]
+house_shown_hand = []
 deck = [11,11,11,11,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
 #print(type(deck[0]))
 #print(player_hand)
 def hit(which_hand):
     "This function takes the player or the bot as the parameter and hits them with a new card from the deck. Removing that card from being drawn in the future."
-    if which_hand == player_hand:
+    if which_hand == "player_hand":
         #print(deck)
         #print(player_hand)
         player_hand.append(deck.pop(random.choice(deck)))
@@ -19,16 +19,16 @@ def hit(which_hand):
         #print(type(player_hand))
         #return player_hand
 
-    elif which_hand == bot_hand:
-        bot_hand.append(deck.pop(random.choice(deck)))
+    elif which_hand == "house_hand":
+        house_hand.append(deck.pop(random.choice(deck)))
         #print(bot_hand)
         #return bot_hand
 
 def game_start():
-    hit(player_hand)
-    hit(bot_hand)
-    hit(player_hand)
-    hit(bot_hand)
+    hit("player_hand")
+    hit("house_hand")
+    hit("player_hand")
+    hit("house_hand")
 
 def play_again():
     another_go = input(f"Would you like to play again?\nType 'y' to go again or 'n' to exit.\n").lower()
@@ -39,14 +39,61 @@ def play_again():
         sys.exit()
 
 def show_hands():
-    bot_shown_hand.append(bot_hand[0])
-    for card in range(1, len(bot_hand)):
-        bot_shown_hand.append(bot_hidden_hand[card])
-    print(f"Dealers hand consists of: {bot_shown_hand}")
+    print(f"Dealers hand consists of: {house_shown_hand}")
     print(f"Your hand consists of: {player_hand}\nAnd a total of {sum(player_hand)} points")
 
-def hit_or_stand():
+def player_hit_or_stand(player_hand):
+    # As long as the player hand is below 21, ask them to hit or stand
+    while sum(player_hand) < 21:
+        # Calls the show hands function that reveals the bot and the players hands.
+        show_hands()
+        player_went_bust = check_bust("player_hand")
+        # Ask for player action as long as the player is not bust
+        if player_went_bust == True:
+            break
+        elif player_went_bust == False:
+            hit_or_stand = input(f"type 'h' for another hit or 's' to stand.\n").lower()
+            if hit_or_stand == "h":
+                hit("player_hand")
+                continue
+            elif hit_or_stand == "s":
+                break
+            else:
+                continue
 
+def house_logic():
+    if sum(house_hand) == 21 and 11 in house_hand and len(house_hand) == 2:
+        stand = True
+        natural = True
+        return stand,natural
+    
+    #elif 
+
+def check_bust(which_hand):
+    if which_hand == "player_hand":
+        if sum(player_hand) > 21 and 11 in player_hand:
+            player_hand[player_hand.index(11)] = 1
+            print(f"Your ace (11) got turned into a 1 so you wouldn't go bust.")
+            return False
+
+        elif sum(player_hand) > 21 and 11 not in player_hand:
+            print(f"Oh, no. You wen't bust! With a total hand score of {sum(player_hand)}.\nLet's see if the house fares any better?")
+            player_bust = True
+            return player_bust
+        else:
+            player_bust = False
+            return player_bust
+    
+    if which_hand == "house_hand":
+        if sum(house_hand) > 21 and 11 in house_hand:
+            house_hand[house_hand.index(11)] = 1
+        
+        if sum(house_hand) > 21 and 11 not in house_hand:
+            print(f"The House wen't bust! With a total hand score of {sum(player_hand)}.\n")
+            house_bust = True
+            return house_bust
+
+########### Main ############
 keep_playing = True
 while keep_playing:
     print("Hello and welcome to BlackJack!")
@@ -54,9 +101,17 @@ while keep_playing:
     if new_game == "y":
         game_start()
     else: sys.exit()
+
+    #Create the dealers hidden hand
+    house_shown_hand.append(house_hand[0])
+    for card in range(1, len(house_hand)):
+        house_shown_hand.append(house_hidden_hand[card])
     
-    # Calls the show hands function that reveals the bot and the players hands.
     show_hands()
+    # In blackjack the players are served first
+    player_hit_or_stand(player_hand)
+
+
 
 
 

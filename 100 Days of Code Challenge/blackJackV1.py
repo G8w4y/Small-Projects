32d614a5190dc32ed9dedf6a2754fe498b70b133
G8w4y <tengelfredheim@gmail.com>
2022-05-18 12:24:07 +0200

    Starting to get control of the house and winning logic

--- 100 Days of Code Challenge/blackJackV1.py
+++ 100 Days of Code Challenge/blackJackV1.py
@@ -42,6 +42,11 @@ def show_hands():
     print(f"Dealers hand consists of: {house_shown_hand}")
     print(f"Your hand consists of: {player_hand}\nAnd a total of {sum(player_hand)} points")
 
+def show_final_hands():
+    print(f"Dealers hand consists of: {house_hand}\nAnd a total of {sum(house_hand)} points")
+    print(f"Your hand consists of: {player_hand}\nAnd a total of {sum(player_hand)} points")
+
+
 def player_hit_or_stand(player_hand):
     # As long as the player hand is below 21, ask them to hit or stand
     while sum(player_hand) < 21:
@@ -64,22 +69,21 @@ def player_hit_or_stand(player_hand):
 def house_logic():
     if check_bust("house_hand") == True:
         stand = True
-        return stand
+        return 'stand'
     if sum(house_hand) == 21 and 11 in house_hand and len(house_hand) == 2:
-        stand = True
         natural = True
-        return stand,natural
+        return 'natural'
     
     elif sum(house_hand) >= 17 and sum(house_hand) < 21:
         stand = True
-        return stand
+        return 'stand'
     
     elif sum(house_hand) < 17:
         hit("house_hand")
     
     else:
         stand = True
-        return stand
+        return 'stand'
 
 def check_bust(which_hand):
     if which_hand == "player_hand":
@@ -112,9 +116,47 @@ def check_bust(which_hand):
             house_bust = False
             return house_bust
 
+def who_won():
+    player_natural = False
+    house_natural = False
+    if sum(player_hand) == 21 and 11 in player_hand and len(player_hand) == 2:
+        player_natural = True
+    
+    if sum(house_hand) == 21 and 11 in house_hand and len(house_hand) == 2:
+        house_natural = True
+
+    if player_natural == True and house_natural == False:
+        print(f"Player won with a BlackJack of {sum(player_hand)}.")
+
+    if player_natural == False and house_natural == True:
+        print(f"The House won with a BlackJack of {sum(player_hand)}.")
+
+    if player_natural == True and house_natural == True:
+        print(f"It's a DRAW! Both parties got a Blackjack of 21 points inlcuding an ace!")
+
+    if sum(player_hand) <=21 and sum(player_hand) > sum(house_hand):
+        print(f"You WON! With a hand consisting of {sum(player_hand)} points vs the Houses hand of {sum(house_hand)} points.")
+    
+    if sum(house_hand) <=21 and sum(house_hand) > sum(player_hand):
+        print(f"You LOSE! With a hand consisting of {sum(player_hand)} points vs the Houses winning hand of {sum(house_hand)} points.")
+    
+    if check_bust('player_hand') == True and check_bust('house_hand') == True:
+        print(f"It's a DRAW! Both parties went bust.")
+
+    if sum(house_hand) == sum(player_hand) and sum(house_hand) <= 21 and sum(player_hand) <= 21 and player_natural == False and house_natural == False:
+        print(f"It's a DRAW! Both parties got {sum(player_hand)} points")
+
+
+
 ########### Main ############
 keep_playing = True
 while keep_playing:
+    player_hand = []
+    house_hand = []
+    house_hidden_hand = ["X","X","X","X","X","X","X",]
+    house_shown_hand = []
+    deck = [11,11,11,11,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+    
     print("Hello and welcome to BlackJack!")
     new_game = input(f"Type 'y' if you would like to play and 'n' if you would like to exit.\n").lower()
     if new_game == "y":
@@ -131,13 +173,18 @@ while keep_playing:
     player_hit_or_stand(player_hand)
 
     # Followed by the house (dealer), who must abide by some special rules.
-    stand = False
-    natural = False
-    while stand != True or natural != True:
-        if house_logic() == natural:
+    stand = ''
+    natural = ''
+    while stand == '' and natural == '':
+        if house_logic() == 'natural':
             natural = house_logic()
-        elif house_logic() == stand:
+        elif house_logic() == 'stand':
             stand = house_logic
+        else:
+            continue
+    
+    show_final_hands()
+    who_won()
 
 
 

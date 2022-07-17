18393f1edbb00512cf852e24a6faaeef879b3b8d
G8w4y <tengelfredheim@gmail.com>
2022-07-17 13:26:54 +0200

    Created a working report function

--- 100 Days of Code Challenge/coffee-machine-challange/main.py
+++ 100 Days of Code Challenge/coffee-machine-challange/main.py
@@ -1,9 +1,21 @@
 from data import MENU
+from data import resources
 # 3 different MENU, each with their unique amount of ingredients
 print(MENU['espresso']['cost'])
 
 # TODO Print report by writing report. How much resources it has left
 
+
+def print_report():
+    for resource in resources.keys():
+        if resource == "water":
+            print(f"{resource.capitalize()} {resources[resource]}ml")
+        elif resource == "milk":
+            print(f"{resource.capitalize()} {resources[resource]}ml")
+        elif resource == "coffee":
+            print(f"{resource.capitalize()} {resources[resource]}g")
+        elif resource == "money":
+            print( f"{resource.capitalize()} ${resources[resource]}")
 # TODO Check if there are sufficient resources within the machine.
 
 # TODO Process coins.
@@ -22,3 +34,13 @@ def process_coins():
 
 # TODO If successfull amount of coins, create the chosen drink and update
 # the resources
+
+
+is_on = True
+while is_on:
+    command = input("What would you like? (espresso/Latte/cappuccino): ").lower()
+    if command == "off":
+        is_on = False
+        break
+    if command == "report":
+        print_report()

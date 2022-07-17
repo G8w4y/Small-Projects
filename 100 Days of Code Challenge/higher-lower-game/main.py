52dd28a9471044a2196cdba44d8efe14a9d8694f
G8w4y <tengelfredheim@gmail.com>
2022-07-17 10:55:46 +0200

    Did some python linting

--- 100 Days of Code Challenge/higher-lower-game/main.py
+++ 100 Days of Code Challenge/higher-lower-game/main.py
@@ -4,14 +4,16 @@ from game_data import data
 import random
 import os
 
-clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 
-HIGHER_LOWER_LIST = ['none','none']
+HIGHER_LOWER_LIST = ['none', 'none']
+
 
 def pick_random_data_entry_from_list():
-    #print(data[random.randint(0,len(data) - 1)])
-    random_entry = data[random.randint(0,len(data) - 1)] #Could also use random.choice(data) here
+    # print(data[random.randint(0,len(data) - 1)])
+    random_entry = data[random.randint(0, len(data) - 1)]  # Could also use random.choice(data) here
     return random_entry
+
 
 def pick_different_starter_entries():
     HIGHER_LOWER_LIST[0] = pick_random_data_entry_from_list()
@@ -19,7 +21,8 @@ def pick_different_starter_entries():
     while HIGHER_LOWER_LIST[0] == HIGHER_LOWER_LIST[1]:
         HIGHER_LOWER_LIST[1] = pick_random_data_entry_from_list()
 
-#Create a function that picks a data_point from the game_data list and displays its key:values in a good manner
+
+# Create a function that picks a data_point from the game_data list and displays its key:values in a good manner
 def display_values():
     entry_a = HIGHER_LOWER_LIST[0]
     print(f"Name: {entry_a['name']}\nDescription: {entry_a['description']} from {entry_a['country']}.\nFollower count: {entry_a['follower_count']} million.")
@@ -27,12 +30,12 @@ def display_values():
     print(vs)
     entry_b = HIGHER_LOWER_LIST[1]
     print(f"Name: {entry_b['name']}\nDescription: {entry_b['description']} from {entry_b['country']}.\nFollower count: ???\nDoes {entry_b['name']} have a higher(1) or Lower(2) follower count than {entry_a['name']}?")
-    #print(type(entry_a))
+    # print(type(entry_a))
 
 
-#Create a function that takes a player guessed input and compares the guess with the follower count of the two data_points
+# Create a function that takes a player guessed input and compares the guess with the follower count of the two data_points
 def guess_and_check():
-    #Check to see if the guessed number is actually a whole number, and nothing else
+    # Check to see if the guessed number is actually a whole number, and nothing else
     while True:
         try:
             player_guess = int(input("Type '1' for Higher and '2' for lower: "))
@@ -54,28 +57,25 @@ def guess_and_check():
     else:
         return False
 
-    #All this can be written alot shorter like this:
+    # All this can be written alot shorter like this:
     # if HIGHER_LOWER_LIST[0]['follower_count'] > HIGHER_LOWER_LIST[1]['follower_count']:
     #     return player_guess == 2
     # else:
     #     return player_guess == 1
 
-    
-    
 
-
-
-#Create a function that swaps data_point A with data_point B and chooses a new and unique random entry for B
+# Create a function that swaps data_point A with data_point B and chooses a new and unique random entry for B
 def swap_and_new_entry():
     HIGHER_LOWER_LIST[0] = HIGHER_LOWER_LIST[1]
     HIGHER_LOWER_LIST[1] = pick_random_data_entry_from_list()
     while HIGHER_LOWER_LIST[0] == HIGHER_LOWER_LIST[1]:
         HIGHER_LOWER_LIST[1] = pick_random_data_entry_from_list()
 
-#Create a loop that continues to ask for guesses for as long as the player guesses correctly
+
+# Create a loop that continues to ask for guesses for as long as the player guesses correctly
 def the_game():
     clear()
-    #Create a welcome statement
+    # Create a welcome statement
     print(logo)
     print("Welcome to the Higher/Lower game!")
     score_counter = 0
@@ -94,31 +94,30 @@ def the_game():
         else:
             print(f"That's too bad. You got the wront guess! {HIGHER_LOWER_LIST[0]['name']} has {HIGHER_LOWER_LIST[0]['follower_count']} million followers,\nwhile {HIGHER_LOWER_LIST[1]['name']} has {HIGHER_LOWER_LIST[1]['follower_count']} million followers.\nFinal score is: {score_counter}")
             still_alive = False
-    
+
         play_again = input("Would you like to play again? ").lower()
         if "y" in play_again:
             pick_different_starter_entries()
             the_game()
-        else: 
+        else:
             break
-
-
-
-#Create a score counter that counts for as long as the player guesses correctly
+# Create a score counter that counts for as long as the player guesses correctly
 
 # for item in range(len(HIGHER_LOWER_LIST)):
 #     print(HIGHER_LOWER_LIST[item])
 
-#swap_and_new_entry()
+# swap_and_new_entry()
 
 # for item in range(len(HIGHER_LOWER_LIST)):
 #     print(HIGHER_LOWER_LIST[item])
 
-#entry_a = HIGHER_LOWER_LIST[0].get('name')
-#print(entry_a)
-#display_values()
+# entry_a = HIGHER_LOWER_LIST[0].get('name')
+# print(entry_a)
+# display_values()
+
+
 clear()
-#Create a welcome statement
+# Create a welcome statement
 print(logo)
 print("Welcome to the Higher/Lower game!")
 ready_to_play = input("The game where you guess who has more or less followers on Instagram than the other! Type 'yes' if you wan't to play and 'no' to exit: ").lower()

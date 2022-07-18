0282f363fb4f8d3a0e875173de49c1eb2926537a
G8w4y <tengelfredheim@gmail.com>
2022-07-18 16:16:25 +0200

    fiddling around with OOP

---
+++ 100 Days of Code Challenge/Day16-start/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+import another_module
+
+print(another_module.another_variable)
+
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+my_screen = Screen()
+timmy.forward(100)
+
+print(my_screen.canvheight)
+my_screen.exitonclick()

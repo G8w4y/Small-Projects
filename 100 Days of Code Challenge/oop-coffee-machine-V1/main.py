cc771491a74e6ce81dceba366968eeff70205335
G8w4y <tengelfredheim@gmail.com>
2022-07-19 17:57:25 +0200

    Strugled abit too follow package documentation. But found the solution in the end.

--- 100 Days of Code Challenge/oop-coffee-machine-V1/main.py
+++ 100 Days of Code Challenge/oop-coffee-machine-V1/main.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -6,19 +6,17 @@ menu = Menu()
 coffee_machine = CoffeeMaker()
 transaction_handler = MoneyMachine()
 
-#print(menu.__getattribute__(MenuItem(menu.find_drink("latte"))))
+is_on = True
 
-print(menu.__getattribute__(str(menu.find_drink("latte"))))
-
-# transaction_handler.process_coins() make_payment
-# is_on = True
-
-# while is_on:
-#     order = input(f"What would you like to order? {menu.get_items()}")
-#     if order == "report":
-#         coffee_machine.report()
-#         transaction_handler.report()
-#     else:
-#         if coffee_machine.is_resource_sufficient(menu.find_drink(order)) is True:
-#             if transaction_handler.make_payment(XXXXX) is True:
-#                 coffee_machine.make_coffee(menu.find_drink(order))
+while is_on:
+    order = input(f"What would you like to order? {menu.get_items()}")
+    if order == "off":
+        is_on = False    
+    elif order == "report":
+        coffee_machine.report()
+        transaction_handler.report()
+    else:
+        coffee_drink = menu.find_drink(order)
+        if coffee_machine.is_resource_sufficient(coffee_drink) is True:
+            if transaction_handler.make_payment(coffee_drink.cost) is True:
+                coffee_machine.make_coffee(coffee_drink)

38f5d341816d3fbf19510230f522e8e5f45462bc
G8w4y <tengelfredheim@gmail.com>
2022-07-22 16:52:28 +0200

    Finished the turtle racing game

--- 100 Days of Code Challenge/Day19-start/Turtle-race-betting/main.py
+++ 100 Days of Code Challenge/Day19-start/Turtle-race-betting/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from random import choice
 
 
 def set_starting_positions():
@@ -19,7 +20,7 @@ turtle7 = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
 
-user_bet = screen.textinput(title="Make Your bet", prompt="Which turtle will win the race? Enter a color: \n'purple'\n'green'\n'red'\n'blue'\n'black'\n'peru'\n'gold'")
+user_bet = screen.textinput(title="Make Your bet", prompt="Which turtle will win the race? Enter a color: \n'purple'\n'green'\n'red'\n'blue'\n'black'\n'peru'\n'gold'").lower()
 print(user_bet)
 
 turtles_list = [turtle1, turtle2, turtle3, turtle4, turtle5, turtle6, turtle7]
@@ -43,6 +44,22 @@ colours_list = [
     "peru",
     "gold"
 ]
+
+race_is_on = True
+
+set_starting_positions()
+
+while race_is_on:
+    for turtle in range(0, len(turtles_list)):
+        if turtles_list[turtle].xcor() == 230:
+            race_is_on = False
+            if user_bet == colours_list[turtle]:
+                print(f"The winner was {colours_list[turtle]}.\nYou bet {user_bet}.\nYou won!")
+            else:
+                print(f"The winner was {colours_list[turtle]}.\nYou bet {user_bet}.\nYou lost!")
+        else:
+            choice(turtles_list).forward(5)
+
 
 screen.listen()
 screen.exitonclick()

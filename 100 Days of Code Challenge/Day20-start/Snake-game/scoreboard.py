49cd583a189d364c0fd80c3ffea2738e9551ba3f
G8w4y <tengelfredheim@gmail.com>
2022-07-24 10:26:49 +0200

    Trying to create and place the boards edges "walls".

--- 100 Days of Code Challenge/Day20-start/Snake-game/scoreboard.py
+++ 100 Days of Code Challenge/Day20-start/Snake-game/scoreboard.py
@@ -19,3 +19,45 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.write(f"SCORE: {self.score}", move=False,align="center",font=("Arial", 20,"normal"))
+
+class Wall(Turtle):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.xcoordinates_list = []
+        self.ycoordinates_list = []
+        self.wall_coordinates_list = []
+        self.walls = []
+        for xcoor in range(-280,281):
+            self.xcoordinates_list.append(xcoor)
+        for ycoor in range(-280,281):
+            self.ycoordinates_list.append(ycoor)
+        
+        # Create the coordinates for the top side of screen
+        for coor in self.xcoordinates_list:
+            self.wall_coordinates_list.append((self.xcoordinates_list[coor], 280))
+        # Create the coordinates for the bottom side of screen
+        for coor in self.xcoordinates_list:
+            self.wall_coordinates_list.append((self.xcoordinates_list[coor], -280))
+        # Create the coordinates for the left side of screen
+        for coor in self.ycoordinates_list:
+            self.wall_coordinates_list.append(-280), (self.ycoordinates_list[coor])
+        # Create the coordinates for the right side of screen
+        for coor in self.ycoordinates_list:
+            self.wall_coordinates_list.append(280), (self.ycoordinates_list[coor])
+        
+        # Create and place every wall piece along the screens edges
+        for coordinate in self.wall_coordinates_list:
+            new_wall = Turtle()
+            new_wall = self.penup
+            new_wall = self.shape("square")
+            new_wall = self.color("grey")
+            new_wall = self.goto(self.wall_coordinates_list[coordinate])
+            self.walls.append(new_wall)
+
+
+        print(self.wall_coordinates_list)
+
+wall = Wall()
+        
